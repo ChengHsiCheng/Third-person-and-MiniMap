ff2238db3e7a570ecc7ea6c4e389f785aaf8d981
ChengHsiCheng <kosakijp@gmail.com>
2022-08-12 16:02:41 +0800

    test

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    float mouseSensitivity = 200f;
+    float mouseSensitivity = 200f;//滑鼠靈敏度
 
-    public Transform playerBody;
+    public Transform playerpos;
 
     void Start()
     {
@@ -15,12 +15,12 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         //旋轉身體的視角
+        //取得滑鼠移動
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         //以玩家為中心旋轉
-        transform.RotateAround(playerBody.transform.position, transform.right, -mouseY);
-        transform.RotateAround(playerBody.transform.position, Vector3.up, mouseX);
+        transform.RotateAround(playerpos.transform.position, transform.right, -mouseY);
+        transform.RotateAround(playerpos.transform.position, Vector3.up, mouseX);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 第三人稱視角 及 小地圖
 ## 第三人稱視角
+![GITHUB](https://i.imgur.com/eJlvJFA.png);
  
